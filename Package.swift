4683c83cbba7dec321ead6075ae97f03a4dca57f
Rainer Killinger <git@killinger.co>
2020-07-01 14:35:53 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,15 @@
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
-  name: "StoreKitCompanion"
+    name: "StoreKitCompanion",
+    products: [
+        .library(name: "StoreKitCompanion", targets: ["StoreKitCompanion"])
+    ],
+    targets: [
+        .target(
+            name: "StoreKitCompanion",
+            path: "StoreKitCompanion"
+        )
+    ]
 )
